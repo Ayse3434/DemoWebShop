e390590cf5b202373349ad7e2f681d2770d54041
Ayse3434 <aysegul2013x@gmail.com>
2023-08-29 15:44:57 +0200

    first commit2

--- src/test/java/com/demowebshop/runners/CukesRunner.java
+++ src/test/java/com/demowebshop/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/demowebshop/stepDefinitions",
         dryRun = false,
-        tags = "@shop"
+        tags = "@shop1"
 )
 
 
